c6fac927980f17d6291978761bad1a0161474dfd
jmockbee <mockbee1000@gmail.com>
2018-03-23 16:29:56 -0400

    last commit on this

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ This is a React.js app that allows you to search for New York Times articles. Wh
 5. yarn start 
 
 ** note the directory is a different set up from the one given in class( create-react-app) .  I find the current set up easier to understand**
+
+According to reaearch for this project I had to add an extra dependency called bluebird to work with mongoose.  
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
+
+## improvements needed
+ 
+ I have to set it up to heroku not sure how to do that yet 
+
+
+having problems with app js unresolved 
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-// search.js 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>New York Times Article Scrubber</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import Main from "./components/Main";
+
+const App = () =>
+  <Router>
+    <div>
+      <Route path="/" component={Main} />
+    </div>
+  </Router>;
 
 export default App;
